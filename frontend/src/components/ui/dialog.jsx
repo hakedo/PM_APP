58ab315f40c16cf582aceb25b273e90a1eb9af59
Edgar Hakobyan <hak3do@gmail.com>
2025-10-18 22:12:58 +0900

    fixed incorrect pointer style for x, cancel, save in the edit pop-up for the templates

--- frontend/src/components/ui/dialog.jsx
+++ frontend/src/components/ui/dialog.jsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef(({ className, children, onClose, ...props
     {children}
     <button
       onClick={onClose}
-      className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none"
+      className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none cursor-pointer"
     >
       <X className="h-4 w-4" />
       <span className="sr-only">Close</span>
